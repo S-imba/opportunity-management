e653c96730bf5e3b38564432d5673470b01483af
S-imba <simbawangulu@gmail.com>
2021-03-04 16:14:18 +0300

    Initiliaze serveless app

--- src/App.js
+++ src/App.js
@@ -1,25 +1,50 @@
-import logo from './logo.svg';
 import './App.css';
+import { EasybaseProvider, useEasybase } from 'easybase';
+import { useEffect } from 'react';
+import ebconfig from './ebconfig';
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App" style={{ display: "flex", justifyContent: "center" }}>
+      <EasybaseProvider ebconfig={ebconfig}>
+        <Notes />
+      </EasybaseProvider>
     </div>
   );
+}
+
+function Notes() {
+  const { Frame, sync, configureFrame } = useEasybase();
+
+  useEffect(() => {
+    configureFrame({ tableName: "NOTES APP", limit : 10})
+    sync();
+  }, []);
+  /*const backendData = [
+    { title: "Grocery List", description: "Milk, Soup, Bread", createdat: "01-18-2021" },
+    { title: "Math Homework", description: "Remember to finish question 8-10 before monday", createdat: "12-01-2020" },
+    { title: "Call James", description: "Ask him about the company party.", createdat: "12-30-2020" }
+  ]*/
+
+  const noteRootStyle = {
+    border: "2px #0af solid",
+    borderRadius: 9,
+    margin: 20,
+    backgroundColor: "#efefef",
+    padding: 6
+  };
+
+  return (
+    <div style={{ width: 400 }}>
+      {backendData.map(ele => 
+        <div style={noteRootStyle}>
+          <h3>{ele.title}</h3>
+          <p>{ele.description}</p>
+          <small>{ele.createdat}</small>
+        </div>
+      )}
+    </div>
+  )
 }
 
 export default App;
